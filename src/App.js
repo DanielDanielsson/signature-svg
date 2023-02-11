c693e2bac4df9a98bc89979456ba5065bf30940c
DanielDanielsson <danielssondaniel94@gmail.com>
2023-02-11 11:26:41 +0100

    init commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./styles/App.css";
+import Signature from "./components/Signature";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="page-wrapper">
+        <Signature />
+      </div>
+    </div>
+  );
+}
+
+export default App;
